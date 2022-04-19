0b32a0b327c066cd61c7e5e6d4c71ea9250a6e3f
_lmmmmmm <931420654@qq.com>
2022-04-20 01:13:29 +0800

    feat: primevue lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
-import { defineNuxtConfig } from 'nuxt3'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineNuxtConfig } from 'nuxt3';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   vite: {
     plugins: [eslintPlugin()]
-  }
-})
+  },
+  css: [
+    'primevue/resources/themes/saga-blue/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css'
+  ]
+});
