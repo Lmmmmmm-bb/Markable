8507e966461cb413d370c65886a79e8ac2f7fd92
_lmmmmmm <931420654@qq.com>
2022-04-19 23:08:49 +0800

    chore: nuxt lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import { defineNuxtConfig } from 'nuxt3'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  vite: {
+    plugins: [eslintPlugin()]
+  }
 })
