4aa62d0dd9c67afffded7e79b39531790d7b328a
_lmmmmmm <931420654@qq.com>
2022-04-20 20:30:44 +0800

    chore: enable devtools and change theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,18 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  vue: {
+    config: {
+      devtools: true
+    }
+  },
   vite: {
     plugins: [eslintPlugin()]
   },
   css: [
     'primevue/resources/themes/lara-light-blue/theme.css',
     'primevue/resources/primevue.css',
-    'primeicons/primeicons.css'
+    'primeicons/primeicons.css',
+    'assets/styles/common/global.scss'
   ]
 });
